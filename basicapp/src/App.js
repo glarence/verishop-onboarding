7cdebf7c7314ff33191207385d636bd91ec57a43
Glarence Zhao <glarencez@gmail.com>
2022-01-06 15:36:18 -0800

    Filtering works, but not as you type, only upon enter or submit

--- basicapp/src/App.js
+++ basicapp/src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './searchBar';
-import { Suspense } from 'react/cjs/react.production.min';
 import { useState } from 'react';
+import { Suspense } from 'react/cjs/react.production.min';
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   const { search } = window.location;
   const query = new URLSearchParams(search).get('s');
   const [searchQuery, setSearchQuery] = useState(query || '');
-  const filteredUsers = filterUsers(users, searchQuery);
+  const users = [
+    { id: '0', firstName: 'James', lastName: 'Sun', userName: 'jamessun', emailAddress: 'jamessun@gmail' }, 
+    { id: '1', firstName: 'Michael', lastName: 'Enferadi', userName: 'michaelenferadi', emailAddress: 'michaelenferadi@gmail' }, 
+    { id: '2', firstName: 'Crystal', lastName: 'Yang', userName: 'crystalyang', emailAddress: 'crystalyang@aol.com'}, 
+    { id: '3', firstName: 'Allie', lastName: 'Scott', userName: 'alliescott', emailAddress: 'alliescott@yahoo.com'},
+  ];
+  const filteredUsers = filterUsernames(users, query);
 
   return (
     <div className="App">
@@ -21,8 +28,11 @@ function App() {
             setSearchQuery={setSearchQuery}
           />
           <ul>
-            {users.map((user) => {
-              <li key={user.key}>{user.firstName + user.lastName}</li>
+            {filteredUsers.map((user) => {
+
+              console.log("Filtered username: " + user.userName + "\n");
+
+              <li key={user.id}>{user.userName}</li>
             })}
           </ul>
       </div>
@@ -30,26 +40,19 @@ function App() {
       </header>
     </div>
   );
-  //HomePage()
-  //UserProfile()
-  //);
 }
 
-const users = [
-  { id: '0', firstName: 'James', lastName: 'Sun', userName: 'jamessun', emailAddress: 'jamessun@gmail' }, 
-  { id: '1', firstName: 'Michael', lastName: 'Enferadi', userName: 'michaelenferadi', emailAddress: 'michaelenferadi@gmail' }, 
-  { id: '2', firstName: 'Crystal', lastName: 'Yang', userName: 'crystalyang', emailAddress: 'crystalyang@aol.com'}, 
-  { id: '3', firstName: 'Allie', lastName: 'Scott', userName: 'alliescott', emailAddress: 'alliescott@yahoo.com'},
-];
-
-const filterUsers = (users, query) => {
+const filterUsernames = (users, query) => {
   if(!query){
+    console.log("invalid query, won't filter");
     return users;
   }
 
   return users.filter((user) => {
-    const userName = user.firstName + user.lastName;
-    return userName.includes(query);
+    console.log("Query is: " + query);
+
+    const username = user.userName.toLowerCase();
+    return username.includes(query);
   });
 };
 
--- basicapp/src/searchBar.js
+++ basicapp/src/searchBar.js
@@ -1,18 +1,36 @@
-const SearchBar = () => {
+import { useNavigate } from 'react-router-dom';
+
+const SearchBar = ({ searchQuery, setSearchQuery }) => {
     //return <div>Hello World!</div>
 
-    return <form action="/" method="get">
-        <label htmlFor="header-search">
-            <span className="visually-hidden">Search for users</span>
-        </label>
-        <input
-            type="text"
-            id="header-search"
-            placeholder="Search users"
-            name="s"
-        />
-        <button type="submit">Search</button>
-    </form>
+    /*const navigate = useNavigate();
+    const onSubmit = e => {
+        e.preventDefault()
+        navigate.push(`?s=${searchQuery}`)
+    };*/
+
+    //  autoComplete="off" onSubmit={onSubmit}
+
+    const handleSubmit = (e) => {
+        
+    }
+
+    return (
+        <form action="/" method="get" onSubmit={handleSubmit}>
+            <label htmlFor="header-search">
+                <span className="visually-hidden">Search for users</span>
+            </label>
+            <input 
+                type="text"
+                id="header-search"
+                placeholder="Search users"
+                name="s"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}>
+            </input>
+            <button type="submit">Search</button>
+        </form>
+    )
 }
 
 export default SearchBar;
