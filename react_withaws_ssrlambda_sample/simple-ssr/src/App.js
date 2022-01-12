4e267f2e8b28fd16ea2465132b085f400ee18f29
Glarence Zhao <glarencez@gmail.com>
2022-01-12 14:22:50 -0800

    Moved over code from basicapp, tested AWS deployment, and client side rendering works same as locally

--- react_withaws_ssrlambda_sample/simple-ssr/src/App.js
+++ react_withaws_ssrlambda_sample/simple-ssr/src/App.js
@@ -6,12 +6,41 @@ import ProductList from "./components/ProductList";
 import config from "./config.json";
 import axios from "axios";
 import SearchBar from './components/SearchBar';
+import UserList from './components/UserList';
+import UserProfile from './components/UserProfile';
 import logo from './logo.svg';
 import './App.css';
+
+const users = [
+  { id: '0', firstName: 'James', lastName: 'Sun', userName: 'jamessun', emailAddress: 'jamessun@gmail' }, 
+  { id: '1', firstName: 'Michael', lastName: 'Enferadi', userName: 'michaelenferadi', emailAddress: 'michaelenferadi@gmail' }, 
+  { id: '2', firstName: 'Crystal', lastName: 'Yang', userName: 'crystalyang', emailAddress: 'crystalyang@aol.com'}, 
+  { id: '3', firstName: 'Allie', lastName: 'Scott', userName: 'alliescott', emailAddress: 'alliescott@yahoo.com'},
+];
+
+const filterUsernames = (users, query) => {
+  if(!query){
+    console.log("invalid query, won't filter");
+    return users;
+  }
+
+  return users.filter((user) => {
+    console.log("Query is: " + query);
+
+    const username = user.userName.toLowerCase();
+    return username.includes(query);
+  });
+};
 
 const App = ({ isSSR, ssrData }) => {
   const [err, setErr] = useState(false);
   const [result, setResult] = useState({ loading: true, products: null });
+
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('s');
+  const [searchQuery, setSearchQuery] = useState(query || '');
+  const filteredUsers = filterUsernames(users, searchQuery);
+
   useEffect(() => {
     const getData = async () => {
       const url = config.SSRApiStack.apiurl;
@@ -29,11 +58,28 @@ const App = ({ isSSR, ssrData }) => {
   } else {
     return (
       <div className="App">
-      <header className="App-header">
-      <div>
-          <SearchBar />
-      </div>
-      </header>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            <SearchBar 
+              searchQuery={searchQuery}
+              setSearchQuery={setSearchQuery}
+            />
+            <h4>List of Users</h4>
+            <hr />
+            <UserList
+              users={filteredUsers}
+            />
+            {filteredUsers.length > 0 &&
+              <UserProfile
+              username={filteredUsers[0].userName}
+              firstname={filteredUsers[0].firstName}
+              lastname={filteredUsers[0].lastName}
+              emailaddress={filteredUsers[0].emailAddress}
+              />
+            }
+          </div>
+        </header>
       </div>
     );
   }
--- react_withaws_ssrlambda_sample/simple-ssr/src/SSRApp.js
+++ react_withaws_ssrlambda_sample/simple-ssr/src/SSRApp.js
@@ -4,20 +4,44 @@
 import React, { useState } from "react";
 import ProductList from "./components/ProductList";
 import SearchBar from './components/SearchBar';
+import UserList from './components/UserList';
+import UserProfile from './components/UserProfile';
 import logo from './logo.svg';
 import './App.css';
 
 const SSRApp = ({ data }) => {
   const [result, setResult] = useState({ loading: false, products: data });
-  return (
-      <div className="App">
-      <header className="App-header">
-      <div>
-          <SearchBar />
-      </div>
 
-      </header>
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('s');
+  const [searchQuery, setSearchQuery] = useState(query || '');
+  const filteredUsers = filterUsernames(users, searchQuery);
+
+  return (
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <SearchBar 
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+        />
+        <h4>List of Users</h4>
+        <hr />
+        <UserList
+          users={filteredUsers}
+        />
+        {filteredUsers.length > 0 &&
+          <UserProfile
+          username={filteredUsers[0].userName}
+          firstname={filteredUsers[0].firstName}
+          lastname={filteredUsers[0].lastName}
+          emailaddress={filteredUsers[0].emailAddress}
+          />
+        }
       </div>
+    </header>
+  </div>
   );
 };
 
