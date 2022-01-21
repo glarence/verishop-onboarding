992e158d703519cbad7fd8493b02b57758e01965
Glarence Zhao <glarencez@gmail.com>
2022-01-21 12:14:56 -0800

    More clean up: not using old searchbar anymore, not filtering searched users manually anymore either, not using hardcoded test user data anymore

--- basicapp/src/App.js
+++ basicapp/src/App.js
@@ -57,32 +57,10 @@ function Users( query, setQuery) {
   }
 }
 
-/*const users = [
-  { id: '0', firstName: 'James', lastName: 'Sun', userName: 'jamessun', emailAddress: 'jamessun@gmail' }, 
-  { id: '1', firstName: 'Michael', lastName: 'Enferadi', userName: 'michaelenferadi', emailAddress: 'michaelenferadi@gmail' }, 
-  { id: '2', firstName: 'Crystal', lastName: 'Yang', userName: 'crystalyang', emailAddress: 'crystalyang@aol.com'}, 
-  { id: '3', firstName: 'Allie', lastName: 'Scott', userName: 'alliescott', emailAddress: 'alliescott@yahoo.com'},
-];*/
-
-/*const filterUsernames = (users, query) => {
-  if(!query){
-    console.log("invalid query, won't filter");
-    return users;
-  }
-
-  return users.filter((user) => {
-    console.log("Query is: " + query);
-
-    const username = user.userName.toLowerCase();
-    return username.includes(query);
-  });
-};*/
-
 function App() {
   //const { search } = window.location;
   //const query = new URLSearchParams(search).get('s');
   //const [searchQuery, setSearchQuery] = useState(query || '');
-  //const filteredUsers = filterUsernames(users, searchQuery);
 
   const [query, setQuery] = useState('');
   const [selectedUser, setSelectedUser] = useState('');
@@ -125,17 +103,5 @@ function SelectUser(){
 //   if(query)
 //     alert('Changed user');
 // }
-
-/*
-<SearchBar 
-              searchQuery={searchQuery}
-              setSearchQuery={setSearchQuery}
-            />
-            <h4>List of Users</h4>
-            <hr />
-            <UserList
-              users={filteredUsers}
-            />
-*/
 
 export default App;
